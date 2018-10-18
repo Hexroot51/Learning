8f709600ecf5901e540618185336a8b065fe364a
Hexroot51 <39087859+Hexroot51@users.noreply.github.com>
2018-10-18 06:38:41 -0400

    Update evolution.c

--- evolution.c
+++ evolution.c
@@ -9,17 +9,17 @@
 int randc(void){                     /* return random printable character */
     return (rand()%(126+1-33)+33);
 }
-void printgen(int gen,char str[50]){ /* Print geeration and string */
+void printgen(int gen,char str[31]){ /* Print geeration and string */
     printf("[%d]:%s\n",gen,str);
 }
 int main(void){
     srand(time(0));
-    char str[50];
-    char randstr[50];
+    char str[31]={'\n'};
+    char randstr[31]={'\n'};
     unsigned int loop;
     int gen=1;
     printf("Enter String:");
-    scanf("%s30",str);
+    scanf("%30s",str);
     for(loop=0;loop<=(strlen(str)-1);loop++){  /* Create Random string */
         randstr[loop]=randc();
     }
